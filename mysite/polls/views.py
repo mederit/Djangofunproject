78ce695ef20cffebfc7f1b11704c1877a66570c2
mederit <iskandar.u.meder@gmail.com>
2021-02-22 18:50:06 +0600

    Отредактировано

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -24,6 +24,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -44,11 +50,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-class DetailView(generic.DetailView):
-    ...
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
